8aff28eade14b2de8b1c44507fc79f675c540351
devGiovanni95 <giovanni.DevJava@hotmail.com>
2024-03-28 09:43:01 -0300

    Modificated teste with fail

--- pilha/src/main/java/org/example/Pilha.java
+++ pilha/src/main/java/org/example/Pilha.java
@@ -1,19 +1,23 @@
 package org.example;
 
 public class Pilha {
+
+    private Object elemento;
+    private int quantidade;
     public boolean estaVazia() {
-        return true;
+        return (elemento == null);
     }
 
     public int tamanho() {
-        return 0;
+        return quantidade;
     }
 
-    public void empilha(String primeiro) {
-
+    public void empilha(Object elemento) {
+    this.elemento = elemento;
+    quantidade++;
     }
 
-    public String topo() {
-        
+    public  Object topo() {
+        return elemento;
     }
 }
